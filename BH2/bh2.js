8039095912e5fef0dbb693cfbc7141d792079317
aaryanrajputhere <119790249+aaryanrajputhere@users.noreply.github.com>
2023-10-24 13:12:37 +0530

    Expanding

--- BH2/bh2.js
+++ BH2/bh2.js
@@ -1,18 +1,4 @@
-import { Analytics } from '@vercel/analytics/react';
- 
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <head>
-        <title>Next.js</title>
-      </head>
-      <body>
-        {children}
-        <Analytics />
-      </body>
-    </html>
-  );
-}
+
 
 let token;
 
