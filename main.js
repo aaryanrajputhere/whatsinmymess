9a4f639a28a5b0d1adff9685cbd13a6e581b48fd
aaryanrajputhere <119790249+aaryanrajputhere@users.noreply.github.com>
2023-10-24 14:56:32 +0530

    NITJ Messes

--- main.js
+++ main.js
@@ -13,4 +13,20 @@ export default function RootLayout({ children }) {
     </html>
   );
 }
+const bh1NITJ = document.getElementById("bh1NITJ")
+const bh2NITJ = document.getElementById("bh2NITJ")
 
+bh2NITJ.addEventListener("click", function () {
+    // Use the following code to load the new HTML file (page2.html)
+    // You can use the Fetch API to load the content of the HTML file
+    fetch("NITJ\bh2.html")
+      .then((response) => response.text())
+      .then((html) => {
+        // Replace the content of a specific element (e.g., a div) with the new HTML content
+        const contentContainer = document.getElementById("contentContainer");
+        contentContainer.innerHTML = html;
+      })
+      .catch((error) => {
+        console.error("Error loading page: " + error);
+      });
+  });
