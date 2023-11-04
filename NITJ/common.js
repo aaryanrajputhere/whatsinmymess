160b8e15720d12acb08f2448391bd40051515f6f
aaryanrajputhere <itsaaryanrajput@gmail.com>
2023-11-04 15:03:50 +0530

    Removed Analytics

--- NITJ/common.js
+++ NITJ/common.js
@@ -1,20 +1,3 @@
-import { Analytics } from '@vercel/analytics/react';
- 
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <head>
-        <title>Next.js</title>
-      </head>
-      <body>
-        {children}
-        <Analytics />
-      </body>
-    </html>
-  );
-}
-
-
 let token;
 
 const currentDate = new Date();
