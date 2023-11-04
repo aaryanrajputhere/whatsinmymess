e6deaa8257de72a3bf198f126aca58373026e126
aaryanrajputhere <itsaaryanrajput@gmail.com>
2023-11-04 12:51:05 +0530

    Added GH 1 & 2 Menu

---
+++ NITJ/gh1&2.js
@@ -0,0 +1,209 @@
+let menu = 
+    [
+        [
+            // Sunday Breakfast
+            "Amritsari Nan & Chole",
+            "Tea",
+            "Milk",
+        ],
+        [
+            // Sunday Lunch
+            "Dal Makhni",
+            "Rice",
+            "Roti",
+            "Salad",
+            "Curd"
+            
+        ],
+        [
+            // Sunday Snacks
+            "Hot Dog",
+        ],
+        [
+            // Sunday Dinner
+            "Kadhai Paneer",
+            "Chicken",
+            "Rice",
+            "Roti",
+            "Salad",
+        ],
+        [
+            // Monday Breakfast
+            "Poha",
+            "Daliya",
+            "Chutney",
+            "Bread Butter",
+            "Tea",
+            "Milk",
+        ],
+        [
+            // Monday Lunch
+            "Khadi",
+            "Aloo Jeera",
+            "Rice",
+            "Roti",
+            "Salad", 
+        ],
+        [
+            // Monday Snacks
+            "Breas Roll / Bread Pakora",
+           
+        ],
+        [
+            // Monday Dinner
+            "Bhindi",
+            "Mix Dal",
+            "Rice",
+            "Roti",
+            "Salad",
+            "Aachar",
+        ],
+        [
+            // Tuesday Breakfast
+            "Gobhi/Mooli Parantha ",
+            "Aachar",
+            "Tea",
+            "Milk",
+            
+            
+        ],
+        [
+            // Tuesday Lunch
+            "Aloo Puri",
+            "Rice",
+            "Roti",
+            "Raita",
+            "Salad",
+        ],
+        [
+            // Tuesday Snacks
+            "Pani Puri",
+        ],
+        [
+            // Tuesday Dinner
+            "Kofte",
+            "Masur Dal",
+            "Roti",
+            "Fried Rice",
+            "Salad",
+            "Rasgulla / Gulab Jamun",
+        ],
+        [
+            // Wednesday Breakfast
+            "Pav Bhaji",
+            "Bread Butter",
+            "Tea",
+            "Milk",
+        ],
+        [
+            // Wednesday Lunch
+            "Rajma",
+            "Rice",
+            "Roti",
+            "Kheera Raita",
+            "Salad",
+            "Aachar"
+        ],
+        [
+            // Wednesday Snacks
+            "Sandwich / Patties",
+        ],
+        [
+            // Wednesday Dinner
+            "Shahi Paneer",
+            "Chicken",
+            "Roti",
+            "Rice",
+            "Salad",
+            "Pastry",
+        ],
+        [
+            // Thursday Breakfast
+            "Aloo Parantha / Bread Sandwich",
+            "Aachar",
+            "Tea",
+            "Milk",
+        ],
+        [
+            // Thursday Lunch
+            "Black Chana",
+            "Rice",
+            "Roti",
+            "Boondi Raita",
+            "Salad",
+            "Aachar",
+        ],
+        [
+            // Thursday Snacks
+            "French Fries",
+            "Shakes",
+        ],
+        [
+            // Thursday Dinner
+            "Moong Dal",
+            "Mic Veg",
+            "Rice",
+            "Roti",
+            "Salad",
+            "Kheer / Jalebi",
+        ],
+        [
+            // Friday Breakfast
+            "Dosa / Uttapam",
+            "Sambhar",
+            "Chatni",
+            "Tea",
+            "Milk",
+        ],
+        [
+            // Friday Lunch
+            "Moong Sabut",
+            "Fried Rice",
+            "Roti",
+            "Curd",
+            "Salad",
+        ],
+        [
+            // Friday Snacks
+            "Nuggets / Samosa",
+        ],
+        [
+            // Friday Dinner
+            "Aloo Beans",
+            "Arhar Dal",
+            "Rice",
+            "Roti",
+            "Salad",
+        ],
+        [
+            // Saturday Breakfast
+            "Macroni",
+            "Bread",
+            "Omlet / Sprouts",
+            "Tea",
+            "Milk",
+        ],
+        [
+            // Saturday Lunch
+            "Chole Bhature",
+            "Rice",
+            "Onion Tomato Raita",
+            "Salad",
+            "Aachar",
+        ],
+        [
+            // Saturday Snacks
+            "Noodles",
+        ],
+        [
+            // Saturday Dinner
+            "Chana Dal",
+            "Aloo Gobhi",
+            "Roti",
+            "Rice",
+            "Salad",
+            "Custard / Halwa",
+        ],
+
+    ]
+
