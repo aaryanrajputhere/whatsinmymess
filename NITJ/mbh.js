d5076db1626a2624a5eecce8ced7e3dd990f5103
aaryanrajputhere <itsaaryanrajput@gmail.com>
2023-11-10 19:22:42 +0530

    Added MBH

---
+++ NITJ/mbh.js
@@ -0,0 +1,259 @@
+let menu = 
+    [
+        [
+            // Sunday Breakfast
+            "Amritsari Naan",
+            "Chole",
+            "Butter",
+            "Tea",
+            "Green Pickle",
+            "Sauce",
+            "Bread Omellate",
+            "Corn Flakes"
+        ],
+        [
+            // Sunday Lunch
+            "Gobhi Aloo Sabzi",
+            "Roti",
+            "Pulao",
+            "Arhar Dal",
+            "Salad(Kheera & Onion)",
+            "Pickle",
+            "Saunf",
+        ],
+        [
+            // Sunday Snacks
+            "Tea",
+        ],
+        [
+            // Sunday Dinner
+            "Chilli Chicken / Chilli Paneer",
+            "Masurdhul Dal",
+            "Rumali Roti",
+            "Rice",
+            "Salad(Onion)",
+            "Pickle",
+            "Saunf",
+        ],
+        [
+            // Monday Breakfast
+            "Chole Bhature",
+            "Tea",
+            "Green Pickle",
+            "Sauce",
+            "Bread Omellate",
+            "Corn Flakes"
+        ],
+        [
+            // Monday Lunch
+            "Rajma",
+            "Roti",
+            "Rice",
+            "Boondi Raita",
+            "Salad(Kheera & Onion)",
+            "Pickle",
+            "Saunf",
+        ],
+        [
+            // Monday Snacks
+            "Sandwich",
+            "Tea"
+           
+        ],
+        [
+            // Monday Dinner
+            "Gajar Matar",
+            "Dal Makhni",
+            "Rice",
+            "Roti",
+            "Kesar Rasmalai",
+            "Salad(Onion)",
+            "Pickle",
+            "Saunf",
+        ],
+        [
+            // Tuesday Breakfast
+            "Poha",
+            "Daliya",
+            "Tea",
+            "Green Pickle",
+            "Sauce",
+            "Bread Omellate",
+            "Corn Flakes"
+        ],
+        [
+            // Tuesday Lunch
+            "Matar Paneer",
+            "Roti",
+            "Mix Dal",
+            "Rice",
+            "Salad(Kheera & Onion)",
+            "Pickle",
+            "Saunf",
+        ],
+        [
+            // Tuesday Snacks
+            "Chana Samosa",
+            "Tea"
+        ],
+        [
+            // Tuesday Dinner
+            "Soaya Chap Chilli",
+            "Roti",
+            "Rice",
+            "Dal Tadka",
+            "Moongdal Halwa",
+            "Salad(Onion)",
+            "Pickle",
+            "Saunf",
+        ],
+        [
+            // Wednesday Breakfast
+            "Aloo Mooli Parantha",
+            "Butter",
+            "Tea",
+            "Green Pickle",
+            "Sauce",
+            "Bread Omellate",
+            "Corn Flakes"
+
+        ],
+        [
+            // Wednesday Lunch
+            "Black Chana Masala",
+            "Fried Rice",
+            "Tadka Dal",
+            "Roti",
+            "Salad(Kheera & Onion)",
+            "Pickle",
+            "Saunf",
+        ],
+        [
+            // Wednesday Snacks
+            "Aloo Patties",
+            "Tea",
+            
+        ],
+        [
+            // Wednesday Dinner
+            "Kadhai Chicken / Kadhai Paneer ",
+            "Tandoori Roti",
+            "Rice",
+            "Mix Dal",
+            "Salad(Onion)",
+            "Pickle",
+            "Saunf",
+        ],
+        [
+            // Thursday Breakfast
+            "Pav Bhaji",
+            "Tea",
+            "Green Pickle",
+            "Sauce",
+            "Bread Omellate",
+            "Corn Flakes"
+        ],
+        [
+            // Thursday Lunch
+            "Aloo Bhujia",
+            "Roti",
+            "Pulao",
+            "Chana Dal",
+            "Papad",
+            "Salad(Kheera & Onion)",
+            "Pickle",
+            "Saunf",
+        ],
+        [
+            // Thursday Snacks
+            "Paneer Kulcha",
+            "Tea",
+        ],
+        [
+            // Thursday Dinner
+            "Gobhi Korma",
+            "Chana Dal",
+            "Roti",
+            "Rice",
+            "Milk Cake",
+            "Salad(Onion)",
+            "Pickle",
+            "Saunf",
+        ],
+        [
+            // Friday Breakfast
+            "Gobhi Parantha",
+            "Butter",
+            "Tea",
+            "Green Pickle",
+            "Sauce",
+            "Bread Omellate",
+            "Corn Flakes"
+        ],
+        [
+            // Friday Lunch
+            "Kadhai Pakoda",
+            "Roti",
+            "Aloo Shimla Mirch",
+            "Rice",
+            "Salad(Kheera & Onion)",
+            "Pickle",
+            "Saunf",
+        ],
+        [
+            // Friday Snacks
+            "Spring Roll",
+            "Tea",
+        ],
+        [
+            // Friday Dinner
+            "Egg Curry / Palak Paneer",
+            "Roti",
+            "Chana Dal",
+            "Pastry",
+            "Salad(Onion)",
+            "Pickle",
+            "Saunf",
+        ],
+        [
+            // Saturday Breakfast
+            "Dosa",
+            "Uttapam",
+            "Chutney",
+            "Sambhar",
+            "Tea",
+            "Green Pickle",
+            "Sauce",
+            "Bread Omellate",
+            "Corn Flakes"
+        ],
+        [
+            // Saturday Lunch
+            "Puri",
+            "Aloo Matar",
+            "Boondi Raita",
+            "Rice",
+            "Salad(Kheera & Onion)",
+            "Pickle",
+            "Saunf",
+        ],
+        [
+            // Saturday Snacks
+            "Hot Dog",
+            "Tomato Soup",
+            "Tea",
+        ],
+        [
+            // Saturday Dinner
+            "Aloo Beans",
+            "Roti",
+            "Tadka Dal",
+            "Rice",
+            "Mal Jaam",
+            "Salad(Onion)",
+            "Pickle",
+            "Saunf",
+        ],
+
+    ]
+
