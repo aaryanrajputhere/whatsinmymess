15d3240eede8ad295693efbf44712eb17ec81584
aaryanrajputhere <itsaaryanrajput@gmail.com>
2023-11-27 11:50:26 +0530

    Menu Update BH2

--- NITJ/bh2.js
+++ NITJ/bh2.js
@@ -1,221 +1,43 @@
-let menu = 
-    [
-        [
-            // Sunday Breakfast
-            "Nan Chhole",
-            "Cornflakes",
-            "Bread Butter & Jam",
-            "Milk",
-        ],
-        [
-            // Sunday Lunch
-            "Aloo Matar Sabji",
-            "Dal",
-            "Rice",
-            "Chapati",
-            "Curd",
-            "Salad",
-        ],
-        [
-            // Sunday Snacks
-            "Coffee",
-        ],
-        [
-            // Sunday Dinner
-            "Kadhai Paneer / Butter Chicken",
-            "Dal",
-            "Rice",
-            "Roti",
-            "Salad",
-            "Rasmalai"
-        ],
-        [
-            // Monday Breakfast
-            "Aloo Parantha",
-            "Butter / Curd",
-            "Cornflakes",
-            "Bread Butter & Jam",
-            "Milk",
-            "Tea",
-        ],
-        [
-            // Monday Lunch
-            "Black Channa",
-            "Chappati",
-            "Rice",
-            "Salad",
-            "Raita",
-        ],
-        [
-            // Monday Snacks
-            "Banana Shake",
-           
-        ],
-        [
-            // Monday Dinner
-            "Mix Veg",
-            "Arhar Daal",
-            "Rice",
-            "Chapati",
-            "Salad",
-            "Choclate Kulfi",
-        ],
-        [
-            // Tuesday Breakfast
-            "Besan Chilla",
-            "Cornflakes",
-            "Bread Butter & Jam",
-            "Milk",
-            "Tea",
-            
-        ],
-        [
-            // Tuesday Lunch
-            "Kadhi Pakoda",
-            "Papad",
-            "Rice",
-            "Chapati",
-            "Salad",
-        ],
-        [
-            // Tuesday Snacks
-            "Aloo Tikki",
-            "Tea"
-        ],
-        [
-            // Tuesday Dinner
-            "Matar Paneer",
-            "Dal Fry",
-            "Rice",
-            "Chapati",
-            "Salad",
-            "Halwa",
-        ],
-        [
-            // Wednesday Breakfast
-            "Poha / Sattu Parantha",
-            "Bread Omelette",
-            "Cornflakes",
-            "Bread Butter & Jam",
-            "Milk",
-            "Tea",
-        ],
-        [
-            // Wednesday Lunch
-            "Rajma",
-            "Rice",
-            "Chapati",
-            "Salad",
-            "Curd",
-        ],
-        [
-            // Wednesday Snacks
-            "Tea",
-        ],
-        [
-            // Wednesday Dinner
-            "Shahi Paneer / Gravy Chicken",
-            "Rice",
-            "Dal",
-            "Butter Naan",
-            "Salad",
-            "Gulab Jamun"
-        ],
-        [
-            // Thursday Breakfast
-            "Chole Bhature",
-            "Cornflakes",
-            "Bread Butter & Jam",
-            "Milk",
-            "Tea",
-        ],
-        [
-            // Thursday Lunch
-            "Soya Chaap",
-            "Maa Sabut Daal",
-            "Rice",
-            "Chapati",
-            "Salad",
-            "Curd",
-        ],
-        [
-            // Thursday Snacks
-            "Paneer Kulche",
-            "Tea",
-        ],
-        [
-            // Thursday Dinner
-            "Chana Masala",
-            "Moong Dal",
-            "Rice",
-            "Chapati",
-            "Salad",
-            "Custard",
-        ],
-        [
-            // Friday Breakfast
-            "Puri",
-            "Aloo Sabzi",
-            "Cornflakes",
-            "Bread Butter & Jam",
-            "Milk",
-            "Tea",
-        ],
-        [
-            // Friday Lunch
-            "Dal Makhani",
-            "Aloo Jeera",
-            "Rice",
-            "Chapati",
-            "Salad",
-        ],
-        [
-            // Friday Snacks
-            "Noodle",
-            "Tea",
-        ],
-        [
-            // Friday Dinner
-            "Egg Curry / Chilli Paneer",
-            "Chilli Souce",
-            "Masar Dal",
-            "Fried Rice",
-            "Chapati",
-            "Salad",
-            "Ice Cream"
-        ],
-        [
-            // Saturday Breakfast
-            "Dosa / Uttapam",
-            "Sambhar",
-            "Moongfali Chatni",
-            "Cornflakes",
-            "Bread Butter & Jam",
-            "Milk",
-            "Tea",
-        ],
-        [
-            // Saturday Lunch
-            "Rajma",
-            "Sambhar",
-            "Rice",
-            "Chapati",
-            "Salad",
-        ],
-        [
-            // Saturday Snacks
-            "Samosa Chana",
-            "Tea",
-        ],
-        [
-            // Saturday Dinner
-            "Aloo Tomato Matar",
-            "Arhar Daal",
-            "Rice",
-            "Chapati",
-            "Salad",
-            "Kheer",
-        ],
+let menu = [
+    // Sunday
+    ["Nan Chhole", "Cornflex", "Bread Butter", "Jam", "Milk"],
+    ["Alloo Jeera", "Dal Makhni", "Dal", "Rice", "Chapati", "Curd", "Salad"],
+    ["Tea"],
+    ["Karahi Paneer", "Chilly Chicken", "Dal", "Rice", "Rotti", "Salad","Rasmalai"],
 
-    ]
+    // Monday
+    ["Aloo Parantha", "Curd", "Cornflex", "Bread Butter", "Jam", "Milk", "Tea"],
+    ["Black Channa", "Chapatti", "Dal", "Fried Rice Salad", "Curd"],
+    ["Patties", "Tea"],
+    ["Nutree Bhurji", "Arhar Dal", "Rice", "Chapatti", "Salad","Moong Dal ka Halwa"],
 
+    // Tuesday
+    ["Puri", "Aloo Sabzi", "Cornflex", "Bread Butter", "Jam", "Milk"],
+    ["Kadi Pakora", "Papad", "Rice", "Chapati", "Salad"],
+    ["Aloo Tikki", "Tea"],
+    ["Aloo Gajar Matar", "Arhar Dal", "Rice", "Chapati", "Salad","Gulab Jamun"],
+
+    // Wednesday
+    ["Poha", "Sprout Chat", "Cornflex", "Bread Omelette", "Butter", "Jam", "Milk"],
+    ["Rajma", "Gheeya Sabzi Rice", "Chapati", "Salad", "Curd"],
+    ["Tea"],
+    ["Shahi Paneer/Gravy Chicken", "Rice", "Dal", "Butter Nan", "Salad", "Gajar Halwa"],
+
+    // Thursday
+    ["Chole Bhature", "Cornflex", "Bread Butter", "Jam", "Milk", "Tea"],
+    ["Allo Methi Sabzi", "Mah Sabat Dal Rice", "Chapatti", "Salad", "Curd"],
+    ["Paneer Kulchhe", "Tea"],
+    ["Sarso ka Saag", "Moong Dal", "Rice", "Makki Roti", "Salad","Chocolate/Vanilla Pastry"],
+
+    // Friday
+    ["Gobi/Methi Pratha", "Cornflex", "Bread Butter", "Jam", "Milk"],
+    ["Dal Makhani", "Baingan ka Bhartha", "Rice", "Chapati", "Papad", "Salad", "Curd"],
+    ["Burger", "Tea",],
+    [ "Egg Curry", "Palak Paneer", "Chilli Souce", "Masar Dal", "Rice", "Chapati", "Salad","Jalebi"],
+
+    // Saturday
+    ["Dosa", "Uttapam", "Sambar", "Moongfali Chatni", "Cornflex", "Bread Butter Jam", "Milk"],
+    ["Alloo Vaddiyan", "Chappati", "Butter", "Salad"],
+    ["French Fries", "Tea",],
+    ["Paneer Bhurji", "Dal Fry", "Rice", "Chapati", "Salad","Khir"],
+]
