0bb8322665e8b3b6c2cb1be3f422279f6255bd23
aaryanrajputhere <itsaaryanrajput@gmail.com>
2024-02-13 20:25:55 +0530

    Update bh5.js

--- NITJ/bh5.js
+++ NITJ/bh5.js
@@ -1,1 +1,50 @@
-let menu = [[]]
+let menu = [
+  // Sunday
+  ["Amritsari Naan", "Cornflakes", "Bread Butter", "Jam", "Tea"],
+  ["Gobhi Matar", "Moong Masoor Dal", "Rice", "Raita", "Salad"],
+  ["Lays", "Biscuit"],
+  ["Kadai Paneer/Chilli Chicken", "Rice", "Roti", "Dal", "Rasmalai"],
+
+  // Monday
+  ["Aloo Pyaz Paratha", "Cornflakes", "Butter", "Jam", "Tea"],
+  ["Kadi Pakoda", "Roti", "Salad", "Papad", "Rice"],
+  ["Aloo Tikki"],
+  ["Simlamirch Veg Biryani", "Dal", "Roti", "Suji Ka Halwa"],
+
+  // Tuesday
+  ["Dosa", "Uttapam", "Cornflakes", "Butter", "Jam", "Tea"],
+  ["Aloo Patta Gobhi", "Massar Sabat Dal", "Rice", "Roti", "Salad", "Raita"],
+  ["Bread Pakoda"],
+  ["Aloo Black Chana", "Roti", "Rice", "Kheer"],
+
+  // Wednesday
+  ["Poha", "Cornflakes", "Butter", "Jam", "Tea"],
+  ["Chole Puri", "Salad", "Rice", "Dahi"],
+  ["Macaroni "],
+  [
+    "Tandoori/Rumali Roti",
+    "Kadai Chicken/Chilli Paneer",
+    "Fried Rice",
+    "Dal",
+    "Roti",
+    "Gulab Jamun",
+  ],
+
+  // Thursday
+  ["Chole Bhature", "Cornflakes", "Butter", "Jam", "Tea"],
+  ["Rajma", "Rice", "Roti", "Lassi", "Salad"],
+  ["Cheese Burger"],
+  ["Mix Veg", "Arhar Dal", "Roti", "Rice", "Jalebi"],
+
+  // Friday
+  ["Bread Omlette", "Cornflakes", "Butter", "Jam", "Tea"],
+  ["Sabhar", "Aloo Jeera", "Dal", "Roti", "Rice", "Salad"],
+  ["Mix Pakoda"],
+  ["Egg Curry/Paneer Butter Masala", "Rice", "Roti", "Pastry"],
+
+  // Saturday
+  ["Pav Bhaji", "Cornflakes", "Butter", "Jam", "Tea"],
+  ["Aloo Matar/Black Chana", "Fried Rice", "Roti", "Salad"],
+  ["Bhel Puri"],
+  ["Matar Mushroom", "Dal", "Rice", "Roti", "Laddu"],
+];
